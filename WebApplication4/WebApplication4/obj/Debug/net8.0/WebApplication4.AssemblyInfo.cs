d9304b2cc9c8c654b068d3a83f3c7af00f15cb97
guyreuveni33 <guyhaim55@gmail.com>
2024-06-01 23:54:57 +0300

    Merge master into main

--- WebApplication4/WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+752ba3ec1f5469601121cb5531933e51aa630162")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
