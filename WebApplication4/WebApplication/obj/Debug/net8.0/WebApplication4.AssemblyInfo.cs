19d903503c163b539d6b0a8b4d78866d60880063
guyreuveni33 <guyhaim55@gmail.com>
2024-06-05 00:34:55 +0300

    rename folder

--- WebApplication4/WebApplication/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/WebApplication/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f9f9f7f2aad22a1b705ec1aa8ed25379b7c26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efaf4aa6fad448128948545334a4ae396805279f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
